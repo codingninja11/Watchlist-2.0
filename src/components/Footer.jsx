839cb29055a58576257f2041763c4be3ebcdc29a
Akriti <akritisheresth@gmail.com>
2022-10-09 23:35:15 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,8 @@
 const Footer = () => {
-  return <div className="footer">Made With ❤ By Abhishek</div>;
+  return <div className="footer">
+    <span>Made With ❤ By Abhishek </span>
+  <h3>Watchlist 2.0<h3>
+  </div>;
 };
 
 export default Footer;
