5fe174980b1a13166b6497c3f6b2e535304afe9f
Akriti <akritisheresth@gmail.com>
2022-10-10 10:27:50 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <header>
       <div className="container">
